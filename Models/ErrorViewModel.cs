a9ce32674487a12a5241f14dc7cbac9db459d2b5
taylor-johnson-codes <taylor.johnson.veteran@outlook.com>
2020-08-24 13:05:17 -0700

    spun up Virtual_Car_Show_Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Virtual_Car_Show_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
