d3cf5709f2a380a19590949ee7fd0422d4f29695
a-garey <agarey@uchicago.edu>
2020-08-26 10:51:12 -0400

    Homepage layout
    Setup for homepage content

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -18,9 +18,35 @@ namespace Virtual_Car_Show_Project.Controllers
         }
 
         [HttpGet("")]
-        public IActionResult Index()
+        public IActionResult HomePage()
         {
+<<<<<<< Updated upstream
             return View();
+=======
+            return View("HomePage");
+        }
+
+        [HttpPost("/charge")]
+        public IActionResult Charge(string StripeEamail, string StripeToken)
+        {
+            var customerService = new CustomerService();
+            var chargeService = new ChargeService();
+
+            var customer = customerService.Create(new CustomerCreateOptions {
+                Email = StripeEamail,
+                Source = StripeToken,
+                
+            });
+
+            var charge = chargeService.Create(new ChargeCreateOptions {
+                Amount = 500,
+                Description = "Sample Charge",
+                Currency = "usd",
+                Customer = customer.Id
+            });
+
+            return View("Success");
+>>>>>>> Stashed changes
         }
 
         
